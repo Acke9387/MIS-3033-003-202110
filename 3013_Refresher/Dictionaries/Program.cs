6cfc53430da80f58055488e80dce7df3d75b8680
Ackerman, Adam P <adamackrmn@gmail.com>
2021-08-30 16:13:52 -0500

    Completed the dictionary problem

---
+++ 3013_Refresher/Dictionaries/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionaries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, double> fruits = new Dictionary<string, double>();
+
+            fruits.Add("Apples", 0.99);
+            fruits.Add("Oranges", 0.50);
+            fruits.Add("Bananas", 0.50);
+            fruits.Add("Grapes", 2.99);
+            fruits.Add("Blueberries", 1.99);
+
+            Console.WriteLine("What fruit would you like the price for? >>");
+            string fruit = Console.ReadLine();
+
+            if (fruits.ContainsKey(fruit) == true)
+            {
+                Console.WriteLine($"The price of {fruit}'s is {fruits[fruit].ToString("C")}");
+
+            }
+            else
+            {
+                Console.WriteLine($"We do not carry {fruit} here.  Go elsewhere.");
+            }
+        }
+    }
+}
