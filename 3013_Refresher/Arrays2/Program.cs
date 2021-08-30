46870325a0d1d9c66acc7d3eca96b4e3e79d1ebd
Ackerman, Adam P <adamackrmn@gmail.com>
2021-08-30 15:59:23 -0500

    completed as much as i will over arrays2

---
+++ 3013_Refresher/Arrays2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Arrays2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] lowercaseLetters = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+            string firstName = "" + lowercaseLetters[0] + lowercaseLetters[3] + lowercaseLetters[0] + lowercaseLetters[12];
+            firstName = lowercaseLetters[0].ToString() + lowercaseLetters[3] + lowercaseLetters[0] + lowercaseLetters[12];
+
+            Console.WriteLine(firstName);
+        }
+    }
+}
