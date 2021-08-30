08d2fdbb03c9e729d5f2dc55b68ee0e080099104
Ackerman, Adam P <adamackrmn@gmail.com>
2021-08-30 16:06:59 -0500

    Completed lists problem

---
+++ 3013_Refresher/Lists/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> examGrades = new List<double>();
+
+            bool shouldIContinue = true;
+
+            while (shouldIContinue)
+            {
+                Console.WriteLine("Please enter your exam grade >>");
+                double grade = Convert.ToDouble(Console.ReadLine());
+
+                examGrades.Add(grade);
+
+                Console.WriteLine("Do you have another grade to enter? yes or no >>");
+
+                if (Console.ReadLine().ToLower() != "yes")
+                {
+                    shouldIContinue = false;
+                    break;
+                }
+            }
+
+            double sum = 0;
+
+            foreach (double grade in examGrades)
+            {
+                sum += grade;
+            }
+
+            Console.WriteLine($"Your average exam grade is {(sum/examGrades.Count).ToString("P")}");
+        }
+    }
+}
