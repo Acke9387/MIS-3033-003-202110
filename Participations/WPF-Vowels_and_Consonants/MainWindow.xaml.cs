56d15ea11cd29e4ace390c47f8f5a4a5f828f86f
Ackerman, Adam P <adamackrmn@gmail.com>
2021-09-08 16:14:36 -0500

    started a position

---
+++ Participations/WPF-Vowels_and_Consonants/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Vowels_and_Consonants
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvert_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (char letter in txtWord.Text.ToLower())
+            {
+                //something somethin goes here
+                lstConsonants.Items.Add(letter);
+
+                //something somethin goes here
+                lstVowels.Items.Add(letter);
+            }
+        }
+    }
+}
