c329d8283544af4a39e0a7de0240d2c5aa4adaab
Ackerman, Adam P <adamackrmn@gmail.com>
2021-09-13 16:06:13 -0500

    did an in class example over classes and WPF

--- In_Class_Examples/WPF_Classes/MainWindow.xaml.cs
+++ In_Class_Examples/WPF_Classes/MainWindow.xaml.cs
@@ -24,5 +24,39 @@ namespace WPF_Classes
         {
             InitializeComponent();
         }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            string id, fname, lname, probation, gpa, balance;
+
+            id = txtID.Text;
+            fname = txtFirstName.Text;
+            lname = txtLastName.Text;
+            //probation = txtProbation.Text;
+            gpa = txtGPA.Text;
+            balance = txtBursar.Text;
+
+            Student stud = new Student(Convert.ToInt32(id), fname, lname, Convert.ToDouble( balance));
+            stud.GPA = Convert.ToDouble(gpa);
+            stud.IsOnProbation = chkProbation.IsChecked.Value;
+            //if (probation.ToLower() == "yes")
+            //{
+            //    stud.IsOnProbation = true;
+            //}
+            //else
+            //{
+            //    stud.IsOnProbation = false;
+            //}
+
+            lstStudents.Items.Add(stud);
+        }
+
+        private void lstStudents_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Student selectedStudent = (Student)lstStudents.SelectedItem;
+
+            MessageBox.Show($"{selectedStudent.FirstName} has a {selectedStudent.GPA.ToString("N2")} GPA!");
+            
+        }
     }
 }
---
+++ In_Class_Examples/WPF_Classes/Student.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPF_Classes
+{
+    public class Student
+    {
+        public int SoonerID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool IsOnProbation { get; set; }
+        public double GPA { get; set; }
+
+        private double BursarBalance;
+
+        //public List<string> Majors { get; set; }
+
+        //public UniversityCourse Course { get; set; }
+
+        /// <summary>
+        /// Default/Empty Constructor
+        /// </summary>
+        public Student()
+        {
+            SoonerID = 0;
+            FirstName = string.Empty;
+            LastName = string.Empty;
+            IsOnProbation = false;
+            GPA = 0;
+            BursarBalance = 0;
+            //Majors = new List<string>();
+            //Course = new UniversityCourse();
+        }
+
+        /// <summary>
+        /// Overloaded constructor
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="fName"></param>
+        /// <param name="lName"></param>
+        /// <param name="BursarBalance"></param>
+        public Student(int id, string fName, string lName, double BursarBalance)
+        {
+            SoonerID = id;
+            FirstName = fName;
+            LastName = lName;
+            IsOnProbation = false;
+            GPA = 0;
+            this.BursarBalance = BursarBalance;
+        }
+
+        public void MakePayment(double amount)
+        {
+
+            if (amount < 0)
+            {
+                Console.WriteLine("Invalid");
+                //throw new Exception("Amount must be greater than 0");
+            }
+            //Assumed x for y
+            BursarBalance -= amount;
+            //BursarBalance = BursarBalance - amount;
+        }
+
+        
+        public double CheckBalance()
+        {
+            return BursarBalance;
+        }
+
+        public override string ToString()
+        {
+            return $"{LastName}, {FirstName} ({SoonerID.ToString("N0")})";
+        }
+
+    }
+
+    //public class UniversityCourse
+    //{
+    //}
+}
