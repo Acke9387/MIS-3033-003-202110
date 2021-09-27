69c4418a8059a5cb6fa1beb22bb0e41ef9f30d07
Ackerman, Adam P <adamackrmn@gmail.com>
2021-09-27 15:22:38 -0500

    Finished contacts participation

---
+++ Participations/WPF_ContactList/Contact.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPF_ContactList
+{
+    public class Contact
+    {
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Photo { get; set; }
+
+
+        public Contact()
+        {
+            Id = 0;
+            FirstName = string.Empty;
+            LastName    = string.Empty;
+            Email       = string.Empty;
+            Photo       = string.Empty;
+        }
+
+        public Contact(int id, string first, string last, string email, string photo)
+        {
+            Id = id;
+            FirstName = first;
+            LastName = last;
+            Email = email;
+            Photo = photo;
+        }
+
+        public override string ToString()
+        {
+            return $"{LastName}, {FirstName}";
+        }
+    }
+}
--- Participations/WPF_ContactList/MainWindow.xaml.cs
+++ Participations/WPF_ContactList/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,33 @@ namespace WPF_ContactList
         public MainWindow()
         {
             InitializeComponent();
+
+            string[] lines = File.ReadAllLines("contacts.txt");
+
+            for (int i = 1; i < lines.Length; i++)
+            {
+                //0      1           2          3       4
+                //Id | FirstName | LastName | Email | Photo
+                //1 | Kerwinn | Moriarty | kmoriarty0@state.gov | https://kelicommheadshots.com/wp-content/uploads/2019/02/Jen-for-Social-Media-2-256x256.jpg
+                string[] pieces = lines[i].Split('|');
+                Contact c = new Contact(Convert.ToInt32(pieces[0]), pieces[1], pieces[2], pieces[3], pieces[4]);
+                // c.FirstName = pieces[1];
+
+                lstContacts.Items.Add(c);
+            }
+
+        }
+
+        private void btnShowDetails_Click(object sender, RoutedEventArgs e)
+        {
+            Contact selected = (Contact)lstContacts.SelectedItem;
+
+            imgPhoto.Source = new BitmapImage(new Uri(selected.Photo));
+
+            txtEmail.Text = selected.Email;
+            txtFirst.Text = selected.FirstName;
+            txtLast.Text = selected.LastName;
+
         }
     }
 }
