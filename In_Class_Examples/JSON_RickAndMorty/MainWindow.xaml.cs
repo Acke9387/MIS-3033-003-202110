903f303ebb7a91a0612332f3004e90c02d579187
Ackerman, Adam P <adamackrmn@gmail.com>
2021-09-27 16:08:30 -0500

    Did an inclass exmaple over rick and morty

---
+++ In_Class_Examples/JSON_RickAndMorty/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JSON_RickAndMorty
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            cboCharacters.Items.Clear();
+
+            using (var client = new HttpClient())
+            {
+
+                string jsonData = client.GetStringAsync("https://rickandmortyapi.com/api/character").Result;
+
+                RickAndMortyAPI api = JsonConvert.DeserializeObject<RickAndMortyAPI>(jsonData);
+                foreach (Character item in api.results)
+                {
+                    cboCharacters.Items.Add(item);
+                }
+            }
+
+        }
+
+        private void cboCharacters_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Character selected = (Character)cboCharacters.SelectedItem;
+
+            imgCharacter.Source = new BitmapImage(new Uri(selected.image));
+            lblName.Content = selected.name;
+
+        }
+    }
+}
---
+++ In_Class_Examples/JSON_RickAndMorty/RickAndMortyAPI.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JSON_RickAndMorty
+{
+    public class RickAndMortyAPI
+    {
+
+        public Info info { get; set; }
+        public List<Character> results { get; set; }
+    }
+
+    public class Character
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string status { get; set; }
+        public string species { get; set; }
+        public string type { get; set; }
+        public string gender { get; set; }
+
+        public string image { get; set; }
+
+        public string url { get; set; }
+
+        public override string ToString()
+        {
+            return $"{name} ({id})";
+        }
+
+    }
+
+    public class Info
+    {
+        public int count { get; set; }
+
+        public int pages { get; set; }
+
+        public string next { get; set; }
+
+        public string prev { get; set; }
+
+    }
+}
