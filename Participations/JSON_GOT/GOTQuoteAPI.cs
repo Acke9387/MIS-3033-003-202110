c0ee2330b1c84dc0cbf607475d2fea4d397f5845
Ackerman, Adam P <adamackrmn@gmail.com>
2021-10-18 15:23:50 -0500

    finished GOT participation and example over using a new window

---
+++ Participations/JSON_GOT/GOTQuoteAPI.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JSON_GOT
+{
+    public class GOTQuoteAPI
+    {
+        public string quote {get;set;}
+        public string character { get; set; }
+    }
+}
---
+++ Participations/JSON_GOT/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JSON_GOT
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<GOTQuoteAPI> Quotes = new List<GOTQuoteAPI>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGetQuote_Click(object sender, RoutedEventArgs e)
+        {
+            GOTQuoteAPI quote;
+            string url = "https://got-quotes.herokuapp.com/quotes";
+
+            using (var client = new HttpClient())
+            {
+                string json = client.GetStringAsync(url).Result;
+
+                quote = JsonConvert.DeserializeObject<GOTQuoteAPI>(json);
+            }
+            txtQuote.Text = quote.quote;
+            lblCharacter.Content = quote.character;
+            Quotes.Add(quote);
+        }
+
+        private void btnExport_Click(object sender, RoutedEventArgs e)
+        {
+            string json = JsonConvert.SerializeObject(Quotes);
+
+            File.WriteAllText("GOT_Quotes.json", json);
+            
+            //Example for using a new window
+            WndExample ex = new WndExample();
+
+            ex.DoSomething();
+
+            ex.Show();
+        }
+    }
+}
