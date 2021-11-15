f2fac2c44753b58a0109be3b72c9915612801075
Ackerman, Adam P <adamackrmn@gmail.com>
2021-11-15 16:08:07 -0600

    did an in class MVC example

---
+++ In_Class_Examples/MVC_Example1/Controllers/OUController.cs
@@ -0,0 +1,41 @@
+﻿using MVC_Example1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Example1.Controllers
+{
+    public class OUController : Controller
+    {
+        // GET: OU
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult TAs()
+        {
+            List<Student> tas = new List<Student>()
+            {
+                new Student()
+                {
+                    FirstName = "Micah",
+                    LastName = "Tison",
+                    SoonerID = 5
+                },
+                new Student()
+                {
+                    FirstName = "Talia",
+                    LastName = "Dean",
+                    SoonerID = 25
+                }
+            };
+
+
+
+            return View(tas);
+        }
+    }
+}
---
+++ In_Class_Examples/MVC_Example1/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Example1.Models
+{
+    public class Student
+    {
+        public string FirstName { get; set; }
+
+        [DisplayName("TA Last Name")]
+        public string LastName { get; set; }
+
+        public int SoonerID { get; set; }
+    }
+}
